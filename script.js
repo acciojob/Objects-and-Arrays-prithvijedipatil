ffe492205f54dc86df894b0fa00f4956f41c2693
prithvijedipatil <prithvipatil09@gmail.com>
2023-06-12 11:32:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,11 +7,7 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+
 
 var team = players;
 const team1 = [...players];
